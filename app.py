f842be69b9c3ae0c28c7387c610537bcf64bf89b
Cyber-Aju <ajmalakram152@gmail.com>
2024-04-04 21:21:51 +0530

    adjust host

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def upload_file():
     return render_template('index.html', message='Error occurred')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
